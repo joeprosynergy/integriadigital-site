509f0d924b3facf5d74f6d19f760caaf4793f489
Joe <159986803+joeprosynergy@users.noreply.github.com>
2025-12-30 13:12:42 +0530

    Enable Astro Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://integria.digital",
-  integrations: [react()],
+  integrations: [
+    react(),
+    tailwind()
+  ],
 });
