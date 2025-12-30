f0e60fab73b02bece673468cc031819817bc1de0
Joe <159986803+joeprosynergy@users.noreply.github.com>
2025-12-30 12:43:28 +0530

    Add React integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://integria.digital"
+  site: "https://integria.digital",
+  integrations: [react()],
 });
